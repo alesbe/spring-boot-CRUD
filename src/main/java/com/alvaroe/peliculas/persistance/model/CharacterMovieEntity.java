aba313d29629196f0b17404fa9c12aa76ade9ff5
Álvaro Esparza <alesbe.dev@gmail.com>
2023-12-14 11:41:03 +0100

    finished jpa entities

--- src/main/java/com/alvaroe/peliculas/persistance/model/CharacterMovieEntity.java
+++ src/main/java/com/alvaroe/peliculas/persistance/model/CharacterMovieEntity.java
@@ -1,27 +1,35 @@
 package com.alvaroe.peliculas.persistance.model;
 
 import com.alvaroe.peliculas.persistance.dao.ActorDAO;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.sql.Connection;
 
-@Getter
-@Setter
+@Entity
+@Table(name = "actors_movies")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CharacterMovieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "characters")
     private String characterName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "actor_id")
     private ActorEntity actorEntity;
 
+    /*
     public ActorEntity getActorEntity(Connection connection, ActorDAO actorDAO) {
         if(actorEntity == null) {
             this.actorEntity = actorDAO.findByCharacterMovieId(connection, id);
         }
 
         return actorEntity;
-    }
+    }*/
 }
